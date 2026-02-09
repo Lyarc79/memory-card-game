b9f7becd16c159337199fbf18f993801a2fd8971
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-02-09 15:43:03 +0100

    Move fetch logic and lift state up to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Scoreboard from "./components/Scoreboard";
 import CardContainer from "./components/CardContainer";
@@ -7,6 +7,8 @@ function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [clickedCards, setClickedCards] = useState([]);
+  const [cards, setCards] = useState([]);
+  const pokemonIds = [1, 5, 15, 50, 150, 250, 350, 450, 550, 650, 750, 850];
 
   const handleCardClick = (cardId) => {
     if (clickedCards.includes(cardId)) {
@@ -21,13 +23,39 @@ function App() {
     setClickedCards((prevCards) => [...prevCards, cardId]);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let pokemonPromises = pokemonIds.map((id) =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        );
+        const responses = await Promise.all(pokemonPromises);
+        const pokemonData = await Promise.all(
+          responses.map((res) => res.json())
+        );
+        const formattedCards = pokemonData.map((pokemon) => ({
+          id: pokemon.id,
+          name: pokemon.name,
+          image: pokemon.sprites.front_default,
+        }));
+        setCards(formattedCards);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  });
+
   return (
     <>
       <Scoreboard
         currentScore={currentScore}
         bestScore={bestScore}
       ></Scoreboard>
-      <CardContainer handleClick={handleCardClick}></CardContainer>
+      <CardContainer
+        handleClick={handleCardClick}
+        cards={cards}
+      ></CardContainer>
     </>
   );
 }
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,33 +1,6 @@
-import { useEffect, useState } from "react";
 import Card from "./Card";
 
-export default function CardContainer({ handleClick }) {
-  const [cards, setCards] = useState([]);
-  const pokemonIds = [1, 5, 15, 50, 150, 250, 350, 450, 550, 650, 750, 850];
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let pokemonPromises = pokemonIds.map((id) =>
-          fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        );
-        const responses = await Promise.all(pokemonPromises);
-        const pokemonData = await Promise.all(
-          responses.map((res) => res.json())
-        );
-        const formattedCards = pokemonData.map((pokemon) => ({
-          id: pokemon.id,
-          name: pokemon.name,
-          image: pokemon.sprites.front_default,
-        }));
-        setCards(formattedCards);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  });
-
+export default function CardContainer({ handleClick, cards }) {
   return (
     <div className="card-grid">
       {cards.map((card) => (
