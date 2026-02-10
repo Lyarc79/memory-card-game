ad9799a830ab05ed8fce1158d92f4d108251c8c8
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-02-10 16:46:08 +0100

    Add loading div and styling

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,16 +1,25 @@
 import Card from "./Card";
 
-export default function CardContainer({ handleClick, cards }) {
+export default function CardContainer({ handleClick, cards, isLoading }) {
   return (
-    <div className="card-grid">
-      {cards.map((card) => (
-        <Card
-          key={card.id}
-          name={card.name}
-          image={card.image}
-          handleClick={() => handleClick(card.id)}
-        ></Card>
-      ))}
-    </div>
+    <>
+      {isLoading ? (
+        <div className="loading-container">
+          <div className="pokeball"></div>
+          <p>Catching new Pokemon...</p>
+        </div>
+      ) : (
+        <div className="card-grid">
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              name={card.name}
+              image={card.image}
+              handleClick={() => handleClick(card.id)}
+            ></Card>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
