bf224a6886adefc72513f1228139fc707ba639de
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-02-18 15:11:24 +0100

    Add title

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,8 +1,14 @@
 export default function Scoreboard({ currentScore, bestScore }) {
   return (
-    <div className="scoreboard">
-      <div>Score:{currentScore}</div>
-      <div>Best Score: {bestScore}</div>
-    </div>
+    <>
+      <h1 className="title">
+        <span className="title-left">Pokémon </span>
+        <span className="title-right">Memory Game</span>
+      </h1>
+      <div className="scoreboard">
+        <div>SCORE: {currentScore}</div>
+        <div>HIGH SCORE: {bestScore}</div>
+      </div>
+    </>
   );
 }
